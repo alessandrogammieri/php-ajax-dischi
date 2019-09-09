9d04b729aa4d037e54640073a8823752d1c7d691
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-09-09 15:39:30 +0200

    initialized project

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- FONT: LATO -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <!-- CSS: MY STYLE -->
+    <link rel="stylesheet" href="style.css">
+    <title>PHP Ajax Dischi musicali</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Ciao Mondo</h1>
+    </div>
+
+    <!-- TEMPLATE: DISCO -->
+    <script id="entry-template" type="text/x-handlebars-template">
+        <div class="disco">
+            
+        </div>
+    </script>
+   
+    <!-- JS: JQUERY -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- JS: HANDLEBARS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+    <!-- JS: MY SCRIPT -->
+    <script src="script.js" charset="utf-8"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,5 @@
+function init() {
+    console.log("Script collegato");
+}
+
+$(document).ready(init);
